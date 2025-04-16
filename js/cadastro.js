d9b32495253676dd563a69f8d68dfb6c8a06b71f
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-16 20:54:40 -0300

    cadastro funcional

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,10 +1,48 @@
+function buscarEnderecoPorCep() {
+	const cep = document.getElementById("cep").value.trim();
+
+	if (cep.length !== 8) {
+		alert("CEP invalido deve conter 8 numeros");
+		return;
+	}
+
+	const url = `https://viacep.com.br/ws/${cep}/json/`;
+
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.erro) {
+				alert("CEP não encontrado");
+				document.getElementById("endereco").value = "";
+				document.getElementById("bairro").value = "";
+				document.getElementById("estado").value = "";
+			} else {
+				const { logradouro, bairro, localidade, uf } = data;
+				document.getElementById("endereco").value = logradouro;
+				document.getElementById("bairro").value = bairro;
+				document.getElementById("estado").value = `${localidade} - ${uf}`;
+			}
+		})
+		.catch((erro) => {
+			console.log("Erro ao buscar CEP", erro);
+			alert("Erro ao buscar CEP");
+		});
+}
+
 function validarCadastro() {
-	const nome = document.getElementById("userfullname").value;
+	const nome = document.getElementById("username").value;
+	const lastName = document.getElementById("userlastname").value;
 	const email = document.getElementById("useremail").value;
 	const senha = document.getElementById("userpass").value;
 	const confirmarSenha = document.getElementById("userpassconfirm").value;
+	//
+	const cep = document.getElementById("cep").value;
+	const endereco = document.getElementById("endereco").value;
+	const bairro = document.getElementById("bairro").value;
+	const estado = document.getElementById("estado").value;
+	const numero = document.getElementById("numero").value;
 
-	if (!nome || !email || !senha || !confirmarSenha) {
+	if (!nome || !lastName || !email || !senha || !confirmarSenha || !cep || !endereco || !bairro || !estado || !numero) {
 		alert("Por favor, preencha todos os campos.");
 		return;
 	}
@@ -14,11 +52,34 @@ function validarCadastro() {
 		return;
 	}
 
-	if (nome === "Admin" && email === "admin@admin" && senha === "admin") {
-		alert("Cadastro bem-sucedido! Redirecionando para a tela de login...");
-		window.location.href = "login.html";
-	} else {
-		alert("Cadastro realizado com sucesso! Redirecionando para a tela de login...");
-		window.location.href = "login.html";
-	}
+	const body = {
+		name: nome,
+		lastName: lastName,
+		email,
+		cep,
+		endereco,
+		bairro,
+		estado,
+		numero: Number(numero),
+		password: senha,
+	};
+
+	const API = "http://localhost:3000/api";
+
+	fetch(`${API}/customers/create`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("Usuário criado com sucesso:", data);
+			window.location.href = "login.html";
+		})
+		.catch((error) => {
+			console.error("Erro ao criar usuário:", error);
+			alert(`Erro ao criar usuário: ${error.message || "Erro desconhecido"}`);
+		});
 }
