21ee01c0cb2d0c2a0bd8b02e991bcdeb0b0e8056
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-17 13:45:16 -0300

    logout

--- js/logout.js
+++ js/logout.js
@@ -1,7 +1,7 @@
 function logout() {
 	localStorage.removeItem("authorization");
 	localStorage.removeItem("role");
-	sessionStorage.removeItem("userId");
+	localStorage.removeItem("user_id");
 	document.cookie = "sessionId=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
 	window.location.href = "index.html";
 }
