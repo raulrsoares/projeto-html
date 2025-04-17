f2964a1a52618d3e8fad6a5c6dfedb0937d5a29f
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-16 21:02:09 -0300

    login funcional

--- js/login.js
+++ js/login.js
@@ -2,18 +2,34 @@ function validarLogin() {
 	const userEmail = document.getElementById("useremail").value;
 	const password = document.getElementById("userpass").value;
 
-	if (userEmail == "" || password == "") {
+	if (!userEmail || !password) {
 		alert("Todos os campos são obrigatórios");
 		return;
 	}
 
-	if (userEmail == "admin@admin" && password == "admin") {
-		console.log("Login efetuado com sucesso!");
-		alert("Login efetuado com sucesso!");
-		window.location.href = "livros.html";
-	} else {
-		console.log("Email ou Senha Inválidos, digite novamente");
-		alert("Email ou Senha Inválidos, digite novamente");
-		return;
-	}
+	const body = {
+		email: userEmail,
+		senha: password,
+	};
+
+	const API = "http://localhost:3000/api";
+
+	fetch(`${API}/auth`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(body),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			console.log("Usuário autenticado com sucesso:", data);
+			sessionStorage.setItem("authorization", data.authorization);
+			sessionStorage.setItem("role", data.role);
+			window.location.href = "livros.html";
+		})
+		.catch((error) => {
+			console.error("Erro ao autenticar:", error);
+			alert(`Erro ao autenticar: ${error.message || "Erro desconhecido"}`);
+		});
 }
