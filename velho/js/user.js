588e05237a4f3bbc2ca7e8c2dad581d145aff3f4
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-17 14:08:33 -0300

    removendo projeto antigo

--- velho/js/user.js
+++
@@ -1,7 +0,0 @@
-function usuarioLogado() {
-	const usuarioLogado = false; // Alterar para `true` se o usuário estiver autenticado
-
-	if (usuarioLogado) {
-		document.getElementById("perfil-link").style.display = "inline-block";
-	}
-}
